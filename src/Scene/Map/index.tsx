bfc564191bdb1d7e0d230faa30ece79f3a6da49b
dmsdn960@gmail.com <dmsdn960@gmail.com>
2019-07-28 12:36:15 +0900

    spot marker, tourist marker on map

--- src/Scene/Map/index.tsx
+++ src/Scene/Map/index.tsx
@@ -5,6 +5,7 @@ import ProfileInfo from './../../Components/ProfileInfo';
 import TravelInfo from './../../Components/TravelInfo';
 import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
+import axios from 'axios';
 //https://docs.expo.io/versions/latest/sdk/imagepicker/
 
 
@@ -21,7 +22,15 @@ export default class Map extends React.Component<Props, State>{
       like: [],
       start_date: '',
       end_date: '',
-      title: ''
+      title: '',
+      tourlist: [],
+      spotlist: [],
+      region: {
+        latitude:37.583176,
+        longitude:126.943280,
+        latitudeDelta: 0.4,
+        longitudeDelta: 0.4
+      }
     }
     static getDerivedStateFromProps(nextProps, preState){
         const {travel_id, name, like, start_date, end_date, title} = nextProps.navigation.state.params;
@@ -38,25 +47,55 @@ export default class Map extends React.Component<Props, State>{
         }
         return null;
     }
+    async componentDidMount(){
+      const spots = await axios.get('https://pic-me-back.herokuapp.com/api/travel/'+this.state.travel_id);
+      const region = spots.data.reduce((acc, cur)=>{
+        acc.latitude+=cur.latitude;
+        acc.longitude+=cur.longitude;
+        return acc;
+      }, {latitude:0,longitude:0,latitudeDelta:0,longitudeDelta:0});
+      region.latitude /= spots.data.length;
+      region.longitude /= spots.data.length;
+      spots.data.forEach(value=>{
+        region.latitudeDelta = Math.max(Math.abs(region.latitude - value.latitude), region.latitudeDelta);
+        region.longitudeDelta = Math.max(Math.abs(region.longitude - value.longitude), region.longitudeDelta);
+      });
+      region.latitudeDelta = region.latitudeDelta==0&&0.04;
+      region.longitudeDelta = region.longitudeDelta==0&&0.04;
+      const result = await axios.get(`https://pic-me-back.herokuapp.com/api/travel/tourist/spot/${region.latitude}/${region.longitude}/${region.latitudeDelta}/${region.longitudeDelta}`);
+      this.setState({
+        spotlist:spots.data,
+        tourlist:result.data,
+        region
+      });
+    }
     render(){
+      const spotlist = this.state.spotlist.map((value, index)=> <Marker
+                    key={index+value._id}
+                    coordinate={{
+                      latitude:value.latitude,
+                      longitude:value.longitude
+                    }}
+                    title={value.title}
+                    pinColor='#58A0FF'
+                    />);
+                    console.log(spotlist)
+      const tourlist = this.state.tourlist.map((value, index) => <Marker 
+                    key={index}
+                    coordinate={{
+                      latitude:value.latitude,
+                      longitude:value.longitude
+                    }}
+                    title={value.name}
+                    description={value.phone}
+                    />);
         return (
           <Fragment>
             <MapView 
             style={{flex:1}}
-              initialRegion={
-                {
-                  latitude:37.583176,
-                  longitude:126.943280,
-                  latitudeDelta: 0.04,
-                  longitudeDelta: 0.04
-                }
-              }>
-                <Marker
-                  coordinate={{
-                    latitude:37.583176,
-                    longitude:126.943280,
-                  }}
-                />
+              region={this.state.region}>
+                {tourlist&&tourlist}
+                {spotlist&&spotlist}
             </MapView>
               <View style={styles.container}>
                 <Header title=''/>
